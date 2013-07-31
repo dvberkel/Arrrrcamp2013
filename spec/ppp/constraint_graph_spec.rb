714dc1d531e4957b7b15e7d0ed0374af56df5ddc
dvberkel <daan.vanberkel@nedap.com>
2013-07-31 15:09:23 +0200

    restructure spec code

--- spec/ppp/constraint_graph_spec.rb
+++ spec/ppp/constraint_graph_spec.rb
@@ -3,22 +3,28 @@ require 'spec_helper'
 require './lib/ppp/constraint_graph'
 require './lib/ppp/constraint_vertex'
 
-describe "ConstraintGraph" do
-  it 'should hold vertices' do
-    graph = PPP::Constraint::Graph.new
+describe 'Constraint' do
+  describe 'Graph' do
+    describe 'Vertices' do
+      it 'should hold vertices' do
+        graph = PPP::Constraint::Graph.new
 
-    graph.addVertex(PPP::Constraint::Vertex.new)
+        graph.addVertex(PPP::Constraint::Vertex.new)
 
-    expect(graph.vertices.size).to be(1)
-  end
+        expect(graph.vertices.size).to be(1)
+      end
+    end
 
-  it 'should connect edges' do
-    graph = PPP::Constraint::Graph.new
-    graph.addVertex(u = PPP::Constraint::Vertex.new)
-    graph.addVertex(v = PPP::Constraint::Vertex.new)
+    describe 'Edges' do
+      it 'should connect edges' do
+        graph = PPP::Constraint::Graph.new
+        graph.addVertex(u = PPP::Constraint::Vertex.new)
+        graph.addVertex(v = PPP::Constraint::Vertex.new)
 
-    graph.addEdge(u, v)
+        graph.addEdge(u, v)
 
-    expect(graph.edges.size).to be(1)
+        expect(graph.edges.size).to be(1)
+      end
+    end
   end
 end

f177a1c2cefeb3829756c22bb1cc59bad53a95f4
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2013-09-24 16:53:51 +0200

    turing machine working

---
+++ js/tm-setup.js
@@ -0,0 +1,31 @@
+(function(tm){
+    document.querySelector('.turing-machine').setAttribute('id', 'turing-machine');
+
+    var word = 'II';
+    var rulebook = {
+        's1' : {
+            'I' : { nextState : 's1', write: 'I', move: 'R' },
+            '' : { nextState : 's2', write: 'I', move: 'L' }
+        },
+        's2' : {
+            'I' : { nextState : 's2', write: 'I', move: 'L' },
+            '' : { nextState : 's1', write: '', move: 'R' }
+        }
+    };
+    var startState = 's1';
+
+    var machine = new tm.Machine(word, rulebook, startState);
+
+    new tm.MachineView('turing-machine', machine, { borderCells: 7, delay: 500 });
+
+    var tape = document.querySelector('.tape');
+    var tapeWidth = tape.offsetWidth;
+    var cells = tape.querySelectorAll('.cell');
+    var cellSize = tapeWidth/cells.length - 2;
+    for (var index = 0; index < cells.length; index++){
+        var cell = cells[index];
+        cell.style.width = "" + cellSize + "px";
+        cell.style.height = "" + cellSize + "px";
+        cell.style.fontSize = "" + 0.8*cellSize + "px";
+    }
+})(tm);
